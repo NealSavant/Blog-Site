8fc2f9023bbf48ebfa460e1e0f8845f57c1a1db0
Neal Savant <nealrsavant@gmail.com>
2020-01-23 11:33:59 -0700

    Stub Boot/MVC project

---
+++ ExtendedLearning/src/main/java/com/skilldistillery/sdelp/ExtendedLearningApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sdelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendedLearningApplication.class, args);
+	}
+
+}
