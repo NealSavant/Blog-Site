92d4e3e2cb941c32aef0931113fd3005810707f4
Frank Martin <cedricmrtn@gmail.com>
2020-01-23 17:23:25 -0700

    Forwarded new DB

--- DB/SD_ELP.sql
+++ DB/SD_ELP.sql
@@ -16,18 +16,6 @@ CREATE SCHEMA IF NOT EXISTS `SD_ELP` DEFAULT CHARACTER SET utf8 ;
 USE `SD_ELP` ;
 
 -- -----------------------------------------------------
--- Table `image`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `image` ;
-
-CREATE TABLE IF NOT EXISTS `image` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `content_img_url` VARCHAR(2000) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -37,31 +25,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `role` VARCHAR(45) NOT NULL DEFAULT 'user',
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
   `active` TINYINT NULL DEFAULT 1,
-  `email` VARCHAR(50) NULL,
-  `images_id` INT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  INDEX `fk_user_images1_idx` (`images_id` ASC),
-  CONSTRAINT `fk_user_images1`
-    FOREIGN KEY (`images_id`)
-    REFERENCES `image` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `content`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `content` ;
-
-CREATE TABLE IF NOT EXISTS `content` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(50) NOT NULL,
-  `content` TEXT NULL,
-  `active` TINYINT NULL DEFAULT 1,
-  PRIMARY KEY (`id`))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
 
 
@@ -72,22 +40,11 @@ DROP TABLE IF EXISTS `topic` ;
 
 CREATE TABLE IF NOT EXISTS `topic` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `images_id` INT NOT NULL,
-  `content_id` INT NOT NULL,
+  `title` VARCHAR(200) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
   `active` TINYINT NULL DEFAULT 1,
-  PRIMARY KEY (`id`),
-  INDEX `fk_content_index_images1_idx` (`images_id` ASC),
-  INDEX `fk_content_index_written_content1_idx` (`content_id` ASC),
-  CONSTRAINT `fk_content_index_images1`
-    FOREIGN KEY (`images_id`)
-    REFERENCES `image` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_content_index_written_content1`
-    FOREIGN KEY (`content_id`)
-    REFERENCES `content` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -100,20 +57,32 @@ CREATE TABLE IF NOT EXISTS `log` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `time_stamp` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
-  `content_index_id` INT NOT NULL,
+  `topic_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_log_user_idx` (`user_id` ASC),
-  INDEX `fk_log_content_index1_idx` (`content_index_id` ASC),
+  INDEX `fk_log_content_index1_idx` (`topic_id` ASC),
   CONSTRAINT `fk_log_user`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_log_content_index1`
-    FOREIGN KEY (`content_index_id`)
+    FOREIGN KEY (`topic_id`)
     REFERENCES `topic` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `image` ;
+
+CREATE TABLE IF NOT EXISTS `image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content_img_url` VARCHAR(5000) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -124,22 +93,128 @@ DROP TABLE IF EXISTS `resource` ;
 
 CREATE TABLE IF NOT EXISTS `resource` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `resource_url` VARCHAR(2000) NOT NULL,
-  `active` TINYINT NULL DEFAULT 1,
-  `images_id` INT NULL,
+  `title` VARCHAR(200) NOT NULL,
+  `resource_url` VARCHAR(5000) NOT NULL,
   `topic_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `update_at` DATETIME NOT NULL,
+  `image_id` INT NULL,
+  `active` TINYINT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  INDEX `fk_resource_images1_idx` (`images_id` ASC),
+  INDEX `fk_resource_images1_idx` (`image_id` ASC),
   INDEX `fk_resource_content_index1_idx` (`topic_id` ASC),
   CONSTRAINT `fk_resource_images1`
-    FOREIGN KEY (`images_id`)
+    FOREIGN KEY (`image_id`)
     REFERENCES `image` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_resource_content_index1`
     FOREIGN KEY (`topic_id`)
     REFERENCES `topic` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `content`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `content` ;
+
+CREATE TABLE IF NOT EXISTS `content` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NOT NULL,
+  `topic_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `update_at` DATETIME NOT NULL,
+  `active` TINYINT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  INDEX `fk_content_topic1_idx` (`topic_id` ASC),
+  CONSTRAINT `fk_content_topic1`
+    FOREIGN KEY (`topic_id`)
+    REFERENCES `topic` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `profile`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `profile` ;
+
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(100) NOT NULL,
+  `last_name` VARCHAR(100) NOT NULL,
+  `image_id` INT NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `user_id` INT NOT NULL,
+  `job_title` VARCHAR(100) NULL,
+  `about` VARCHAR(2500) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_profile_image1_idx` (`image_id` ASC),
+  INDEX `fk_profile_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_profile_image1`
+    FOREIGN KEY (`image_id`)
+    REFERENCES `image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_profile_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `topic_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `topic_comment` ;
+
+CREATE TABLE IF NOT EXISTS `topic_comment` (
+  `id` BIGINT(20) NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `update_at` DATETIME NOT NULL,
+  `topic_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `content` TEXT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_post_topic1_idx` (`topic_id` ASC),
+  INDEX `fk_post_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_post_topic1`
+    FOREIGN KEY (`topic_id`)
+    REFERENCES `topic` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `topic_image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `topic_image` ;
+
+CREATE TABLE IF NOT EXISTS `topic_image` (
+  `topic_id` INT NOT NULL,
+  `image_id` INT NOT NULL,
+  PRIMARY KEY (`topic_id`, `image_id`),
+  INDEX `fk_topic_image_image1_idx` (`image_id` ASC),
+  CONSTRAINT `fk_topic_image_topic1`
+    FOREIGN KEY (`topic_id`)
+    REFERENCES `topic` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_topic_image_image1`
+    FOREIGN KEY (`image_id`)
+    REFERENCES `image` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -160,7 +235,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `SD_ELP`;
-INSERT INTO `user` (`id`, `username`, `password`, `role`, `active`, `email`, `images_id`) VALUES (1, 'admin', 'admin', 'ADMIN', 1, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `role`, `created_at`, `updated_at`, `active`) VALUES (1, 'admin', 'admin', 'ADMIN', '2020-01-23', '2020-01-23', 1);
 
 COMMIT;
 
