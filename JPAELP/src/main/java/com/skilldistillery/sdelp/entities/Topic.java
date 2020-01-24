cf880f4d1f0e9c86f09e1bdde335739f18b5666f
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-23 19:20:48 -0700

    update topic entity, change mapping for image to list

--- JPAELP/src/main/java/com/skilldistillery/sdelp/entities/Topic.java
+++ JPAELP/src/main/java/com/skilldistillery/sdelp/entities/Topic.java
@@ -1,15 +1,22 @@
 package com.skilldistillery.sdelp.entities;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 @Entity
@@ -19,13 +26,26 @@ public class Topic {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	public String title;
+	
+	@Column(name="created_at")
+	@CreationTimestamp
+	public LocalDateTime createdAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	public LocalDateTime updatedAt;
+	
 	@OneToOne
 	@JoinColumn(name="content_id")
 	public Content content;
 	
-	@OneToOne
-	@JoinColumn(name="images_id")
-	public Image image;
+	
+	@ManyToMany
+	@JoinTable(name="topic_image",
+			joinColumns=@JoinColumn(name="topic_id"),
+			inverseJoinColumns=@JoinColumn(name="image_id"))
+	public List<Image> images;
 	
 	private Boolean active;
 	
@@ -40,6 +60,30 @@ public class Topic {
 		this.id = id;
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
 	public Content getContent() {
 		return content;
 	}
@@ -48,12 +92,13 @@ public class Topic {
 		this.content = content;
 	}
 
-	public Image getImage() {
-		return image;
+	public List<Image> getImages() {
+		List<Image> copy = new ArrayList<>(images);
+		return copy;
 	}
 
-	public void setImage(Image image) {
-		this.image = image;
+	public void setImages(List<Image> images) {
+		this.images = images;
 	}
 
 	public Boolean getActive() {
@@ -72,6 +117,36 @@ public class Topic {
 	public void setResources(List<Resource> resources) {
 		this.resources = resources;
 	}
+
+	@Override
+	public String toString() {
+		return "Topic [id=" + id + ", title=" + title + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
+				+ ", content=" + content + ", active=" + active + ", resources=" + resources + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
 	
 	
 	
