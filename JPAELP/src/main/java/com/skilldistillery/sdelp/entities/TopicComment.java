8ede56c1fb289d5eb747905d60eed6fe04644bbf
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-23 20:23:56 -0700

    add topiccomment entity

---
+++ JPAELP/src/main/java/com/skilldistillery/sdelp/entities/TopicComment.java
@@ -0,0 +1,131 @@
+package com.skilldistillery.sdelp.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="topic_comment")
+public class TopicComment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	@Column(name="created_at")
+	@CreationTimestamp
+	public LocalDateTime createdAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	public LocalDateTime updatedAt;
+	
+	@OneToOne
+	@JoinColumn(name="topic_id")
+	private Topic topic;
+	
+	@OneToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "TopicComment [id=" + id + ", title=" + title + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
+				+ ", topic=" + topic + ", user=" + user + ", content=" + content + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TopicComment other = (TopicComment) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+}
