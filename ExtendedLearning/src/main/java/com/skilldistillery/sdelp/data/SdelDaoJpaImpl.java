c07851067a047918f774ade12d1cf7b19536f02d
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-24 09:00:34 -0700

    stub dao impl from dao interface

---
+++ ExtendedLearning/src/main/java/com/skilldistillery/sdelp/data/SdelDaoJpaImpl.java
@@ -0,0 +1,111 @@
+package com.skilldistillery.sdelp.data;
+
+import java.util.List;
+
+import com.skilldistillery.sdelp.entities.Content;
+import com.skilldistillery.sdelp.entities.Image;
+import com.skilldistillery.sdelp.entities.Log;
+import com.skilldistillery.sdelp.entities.Resource;
+import com.skilldistillery.sdelp.entities.Topic;
+import com.skilldistillery.sdelp.entities.TopicComment;
+import com.skilldistillery.sdelp.entities.User;
+
+public class SdelDaoJpaImpl implements SdelDao {
+
+	@Override
+	public User getUserByUsernamePassword(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User createUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Topic> findTopicsBySearchTerm(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Topic> getAllTopics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Topic getTopicById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Topic addTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Topic updateTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource addResource(Resource resource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource updateResource(Resource resource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Content addContent(Content content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Content updateContent(Content content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image addImage(Image image) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Log writeLog(Log log) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TopicComment addTopicComment(TopicComment topicComment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TopicComment updateTopicComment(TopicComment topicComment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
