458461ee20c6ccbcae3828b51bb81a81b1166c4a
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-24 11:38:59 -0700

    break out image dao from main, delete old main dao since it is now empty

--- ExtendedLearning/src/main/java/com/skilldistillery/sdelp/data/ImageDAOJpaImpl.java
+++ ExtendedLearning/src/main/java/com/skilldistillery/sdelp/data/ImageDAOJpaImpl.java
@@ -1,42 +1,24 @@
 package com.skilldistillery.sdelp.data;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.skilldistillery.sdelp.entities.Content;
 import com.skilldistillery.sdelp.entities.Image;
-import com.skilldistillery.sdelp.entities.Log;
-import com.skilldistillery.sdelp.entities.Resource;
-import com.skilldistillery.sdelp.entities.Topic;
-import com.skilldistillery.sdelp.entities.TopicComment;
-
 
 @Transactional
 @Service
-public class SdelDaoJpaImpl implements SdelDao {
-	
+public class ImageDAOJpaImpl implements ImageDAO {
+
 	@PersistenceContext
 	private EntityManager em;
-
-
-	
-
-
-
 
 	@Override
 	public Image addImage(Image image) {
 		em.persist(image);
 		return image;
 	}
-
-
-
-
 
 }
