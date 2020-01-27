808f69b9d02a142fd1c87ace97cb9ef75a511474
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-27 09:40:32 -0700

    add show/attempt for new topic and update topic

--- ExtendedLearning/src/main/java/com/skilldistillery/sdelp/controllers/TopicController.java
+++ ExtendedLearning/src/main/java/com/skilldistillery/sdelp/controllers/TopicController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.sdelp.data.TopicDAO;
@@ -18,7 +19,7 @@ public class TopicController {
 	@Autowired
 	TopicDAO topicdao = new TopicDAOJpaImpl();
 	
-	@RequestMapping(path="showTopicList.do")
+	@RequestMapping(path="showSingleTopic.do")
 	public String showTopicList(@RequestParam int cid, Model model) {
 		Topic topic = topicdao.getTopicById(cid);
 		model.addAttribute("topic", topic);
@@ -39,5 +40,30 @@ public class TopicController {
 		model.addAttribute("topics", topics);
 		return "keywordTopics";
 	}
-
+	
+	@RequestMapping(path="showNewTopic.do")
+	public String showNewTopic() {
+		return "newTopic";
+	}
+	
+	@RequestMapping(path="attemptCreateTopic.do", method = RequestMethod.POST)
+	public String attemptCreateTopic(Model model) {
+		
+		
+		return "topic";
+	}
+	
+	@RequestMapping(path="showUpdateTopic.do")
+	public String updateTopic() {
+		
+		
+		return "updateTopic";
+	}
+	
+	@RequestMapping(path="attemptUpdateTopic.do", method = RequestMethod.POST)
+	public String attemptUpdateTopic(Model model) {
+		
+		
+		return "topic";
+	}
 }
